21f27eb8aeaf6679b8cdf0cefd4e10e51fab876a
Damodhar Reddy Pulluru <Damodhar.Pulluru.Contractor@pepsico.com>
2023-01-03 22:39:31 +0530

    complete

---
+++ src/Other/Questions.js
@@ -0,0 +1,26 @@
+export const Questions = [
+  {
+    prompt: "DOM full form?",
+    optionA: "Document Object Model",
+    optionB: "Dance on moon",
+    optionC: "Dolo 650",
+    optionD: "None of the above",
+    asnwer: "optionA",
+  },
+  {
+    prompt: "Which of this is not a programming language?",
+    optionA: "Python",
+    optionB: "JavaScript",
+    optionC: "English",
+    optionD: "Java",
+    asnwer: "optionC",
+  },
+  {
+    prompt: "Which of this is not a javascript framework?",
+    optionA: "React",
+    optionB: "Angular",
+    optionC: "Vue",
+    optionD: "Java",
+    asnwer: "optionD",
+  },
+];
---
+++ src/components/Menu.js
@@ -0,0 +1,28 @@
+import "../App.css";
+import { useContext } from "react";
+import { GameStateContext } from "../Other/Contexts";
+
+function Menu() {
+  const { setGameState, setUserName } = useContext(GameStateContext);
+  return (
+    <div className="Menu">
+      <label>Enter Your Name:</label>
+      <input
+        type="text"
+        placeholder="Ex. Damodhar"
+        onChange={(event) => {
+          setUserName(event.target.value);
+        }}
+      />
+      <button
+        onClick={() => {
+          setGameState("playing");
+        }}
+      >
+        Start Quiz
+      </button>
+    </div>
+  );
+}
+
+export default Menu;
